9df293bab7d32234169326df76c4cd21ade03fa2
dmalitesta <d.malitesta@gmail.com>
2023-12-28 23:31:07 +0100

    First commit.

--- start_experiments.py
+++ start_experiments.py
@@ -175,7 +175,7 @@ else:
         validation_rate: 10
         validation_metric: Recall@20
         restore: False
-            batch_size: 1024
+      batch_size: 1024
       factors: 64
       lr: 0.001
       l_w: 1e-5
