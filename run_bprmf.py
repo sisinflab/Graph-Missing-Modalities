ba3b9038a9f4fb5bc35d72a2edbb2ef8ee115f18
danielemalitesta <d.malitesta@gmail.com>
2024-05-03 17:30:03 +0200

    Imputation with separate file.

--- run_bprmf.py
+++ run_bprmf.py
@@ -2,8 +2,7 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
-parser.add_argument('--dataset', type=str, default='bprmf')
-parser.add_argument('--model', type=str, default='Digital_Music')
+parser.add_argument('--data', type=str, default='Digital_Music')
 args = parser.parse_args()
 
-run_experiment(f"config_files/{args.model}_{args.dataset}.yml")
+run_experiment(f"config_files/bprmf_{args.dataset}.yml")
