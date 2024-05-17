4587b49b23a0253b2c22f9178096863caf5a3358
Daniele Malitesta <57352389+danielemalitesta@users.noreply.github.com>
2024-05-17 21:33:26 +0200

    Update collect_results.sh

--- collect_results.sh
+++ collect_results.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep "Best Model results" $HOME/workspace/Graph-Missing-Modalities/logs/$1/$2/*.out > feat_prop_$1_$2_collected.out
+grep "Best Model results" logs/$1/$2/*.log > feat_prop_$1_$2_collected.out
