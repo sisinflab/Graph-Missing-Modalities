c462d3e0592a3dd7246f7d6b0cd1fedb032cd97f
danielemalitesta <d.malitesta@gmail.com>
2025-01-12 12:11:55 +0100

    Added graph autoencoder as imputation method. Needs to be tested.

---
+++ impute_all_gae.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import ParameterGrid
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', type=str, default='Baby', help='choose the dataset')
+parser.add_argument('--gpu_id', type=int, default=0, help='choose gpu id')
+args = parser.parse_args()
+
+hyperparams = ParameterGrid({
+    "--top_k": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+})
+
+bash = "#!/bin/bash\n"
+
+for hyp in hyperparams:
+    bash += (f"python impute_autoencoder.py "
+             f"--data {args.data} "
+             f"--gpu {args.gpu_id} "
+             f"--top_k {hyp['--top_k']} "
+             f"--method gae\n")
+
+with open(f"impute_all_gae_{args.data}.sh", 'w') as f:
+    f.write(bash)
--- impute_autoencoder.py
+++ impute_autoencoder.py
@@ -3,13 +3,14 @@ import os
 import pandas as pd
 import numpy as np
 import torch
-from torch_sparse import SparseTensor, matmul
+from torch_sparse import SparseTensor
 import scipy.sparse as sp
 
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 import random
+from torch_geometric.nn import GCNConv
 
 random.seed(42)
 np.random.seed(42)
@@ -59,11 +60,52 @@ class AutoEncoderTextual2Visual(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
+class GraphEncoderVisual2Textual(torch.nn.Module):
+    def __init__(self):
+        super(GraphEncoderVisual2Textual, self).__init__()
+        self.conv1 = GCNConv(2048, 1024)
+        self.conv2 = GCNConv(1024, 512)
+        self.decoder = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 768),
+            nn.Sigmoid()
+        )
 
-def get_item_item():
+    def message_passing(self, x, edge_index):
+        encoded = self.conv1(x, edge_index)
+        encoded = torch.nn.functional.leaky_relu(encoded)
+        encoded = self.conv2(encoded, edge_index)
+        encoded = torch.nn.functional.leaky_relu(encoded)
+        return encoded
+
+    def forward(self, encoded):
+        decoded = self.decoder(encoded)
+        return decoded
+
+class GraphEncoderTextual2Visual(torch.nn.Module):
+    def __init__(self):
+        super(GraphEncoderTextual2Visual, self).__init__()
+        self.conv1 = GCNConv(768, 1024)
+        self.decoder = nn.Sequential(
+            nn.Linear(1024, 2048),
+            nn.Sigmoid()
+        )
+
+    def message_passing(self, x, edge_index):
+        encoded = self.conv1(x, edge_index)
+        encoded = torch.nn.functional.leaky_relu(encoded)
+        return encoded
+
+    def forward(self, encoded):
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+def get_item_item(num_items):
     # compute item_item matrix
     user_item = sp.coo_matrix(([1.0] * len(train), (train[0].tolist(), train[1].tolist())),
-                              shape=(train[0].nunique(), num_items_visual), dtype=np.float32)
+                              shape=(train[0].nunique(), num_items), dtype=np.float32)
     item_item = user_item.transpose().dot(user_item).toarray()
     knn_val, knn_ind = torch.topk(torch.tensor(item_item, device=device), args.top_k, dim=-1)
     items_cols = torch.flatten(knn_ind).to(device)
@@ -78,9 +120,7 @@ def get_item_item():
 
 parser = argparse.ArgumentParser(description="Run imputation.")
 parser.add_argument('--data', type=str, default='Office_Products')
-parser.add_argument('--gpu', type=str, default='0')
-parser.add_argument('--layers', type=int, default=1)
-parser.add_argument('--method', type=str, default='ae')
+parser.add_argument('--method', type=str, default='gae')
 parser.add_argument('--top_k', type=int, default=20)
 args = parser.parse_args()
 
@@ -230,22 +270,40 @@ elif args.method == 'gae':
     visual_shape = np.load(os.path.join(visual_folder, os.listdir(visual_folder)[0])).shape
     textual_shape = np.load(os.path.join(textual_folder, os.listdir(textual_folder)[0])).shape
 
+    num_items_visual = len(os.listdir(visual_folder))
+    num_items_textual = len(os.listdir(textual_folder))
+
+    visual_items = os.listdir(visual_folder)
+    textual_items = os.listdir(textual_folder)
+
+    all_present = [x for x in textual_items if x in visual_items]
+
+    visual_features = np.empty((len(all_present), visual_shape[-1]), dtype=np.float32) if num_items_visual else None
+    textual_features = np.empty((len(all_present), textual_shape[-1]), dtype=np.float32) if num_items_textual else None
+
+    inner_map_item = {}
+
+    for idx, it in enumerate(all_present):
+        inner_map_item[it] = idx
+        visual_features[idx, :] = np.load(os.path.join(visual_folder, it))
+        textual_features[idx, :] = np.load(os.path.join(textual_folder, it))
+
     output_visual = f'data/{args.data}/visual_embeddings_{args.method}_{args.top_k}_indexed'
     output_textual = f'data/{args.data}/textual_embeddings_{args.method}_{args.top_k}_indexed'
 
     try:
         train = pd.read_csv(f'data/{args.data}/train_indexed.tsv', sep='\t', header=None)
+        train = train[train[1].isin(all_present)]
     except FileNotFoundError:
-        print('Before imputing through feat_prop, split the dataset into train/val/test!')
+        print('Before imputing through gae, split the dataset into train/val/test!')
         exit()
 
-    num_items_visual = len(missing_visual) + len(os.listdir(visual_folder))
-    num_items_textual = len(missing_textual) + len(os.listdir(textual_folder))
+    inner_map_user = {u: idx for idx, u in enumerate(train[0].unique())}
 
-    visual_features = torch.zeros((num_items_visual, visual_shape[-1]))
-    textual_features = torch.zeros((num_items_textual, textual_shape[-1]))
+    train[0] = train[0].map(inner_map_user)
+    train[1] = train[1].map(inner_map_item)
 
-    adj = get_item_item()
+    adj = get_item_item(num_items=len(all_present))
 
     try:
         missing_visual_indexed = pd.read_csv(os.path.join(f'data/{args.data}', 'missing_visual_indexed.tsv'), sep='\t',
@@ -261,34 +319,131 @@ elif args.method == 'gae':
     except (pd.errors.EmptyDataError, FileNotFoundError):
         missing_textual_indexed = set()
 
-    # feat prop on visual features
-    for f in os.listdir(f'data/{args.data}/visual_embeddings_indexed'):
-        visual_features[int(f.split('.npy')[0]), :] = torch.from_numpy(
-            np.load(os.path.join(f'data/{args.data}/visual_embeddings_indexed', f)))
+    # train GAE to reconstruct textual through visual
+    model_impute_textual = GraphEncoderVisual2Textual()
+    model_impute_textual.to(device)
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model_impute_textual.parameters(), lr=1e-4)
 
-    non_missing_items = list(set(list(range(num_items_visual))).difference(missing_visual_indexed))
-    propagated_visual_features = visual_features.clone()
+    visual_normalized = torch.from_numpy(visual_features)
+    visual_normalized = visual_normalized - torch.from_numpy(visual_features).min(0, keepdim=True)[0]
+    visual_normalized = visual_normalized / (torch.from_numpy(visual_features).max(0, keepdim=True)[0] -
+                                             torch.from_numpy(visual_features).min(0, keepdim=True)[0])
+    visual_normalized.to(device)
 
-    for idx in range(args.layers):
-        print(f'[VISUAL] Propagation layer: {idx + 1}')
-        propagated_visual_features = matmul(adj.to(device), propagated_visual_features.to(device))
-        propagated_visual_features[non_missing_items] = visual_features[non_missing_items].to(device)
+    textual_normalized = torch.from_numpy(textual_features)
+    textual_normalized = textual_normalized - torch.from_numpy(textual_features).min(0, keepdim=True)[0]
+    textual_normalized = textual_normalized / (torch.from_numpy(textual_features).max(0, keepdim=True)[0] -
+                                               torch.from_numpy(textual_features).min(0, keepdim=True)[0])
+    textual_normalized.to(device)
 
+    items = torch.arange(visual_normalized.shape[0])
+
+    dataset = TensorDataset(items, textual_normalized)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    cumulative_loss = 0
+    for epoch in range(1000):
+        for batch in dataloader:
+            features = model_impute_textual.message_passing(visual_normalized.to(device), adj.to(device))
+            items_id, targets = batch
+            outputs = model_impute_textual.forward(features[items_id].to(device))
+            loss = criterion(outputs.to(device), targets.to(device))
+            cumulative_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if (epoch + 1) % 100 == 0:
+            print(f"Epoch {epoch + 1}, Loss: {cumulative_loss / 100:.4f}")
+            cumulative_loss = 0
+
+    # train GAE to reconstruct visual through textual
+    model_impute_visual = GraphEncoderTextual2Visual()
+    model_impute_visual.to(device)
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model_impute_visual.parameters(), lr=1e-4)
+
+    dataset = TensorDataset(items, visual_normalized)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    cumulative_loss = 0
+    for epoch in range(1000):
+        for batch in dataloader:
+            features = model_impute_visual.message_passing(textual_normalized.to(device), adj.to(device))
+            items_id, targets = batch
+            outputs = model_impute_visual.forward(features[items_id].to(device))
+            loss = criterion(outputs.to(device), targets.to(device))
+            cumulative_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if (epoch + 1) % 100 == 0:
+            print(f"Epoch {epoch + 1}, Loss: {cumulative_loss / 100:.4f}")
+            cumulative_loss = 0
+
+    # impute missing visual from textual
     for miss in missing_visual_indexed:
-        np.save(os.path.join(output_visual, f'{miss}.npy'), propagated_visual_features[miss].detach().cpu().numpy())
+        if miss in missing_textual_indexed:
+            np.save(os.path.join(output_visual, f'{miss}.npy'), np.zeros(visual_shape, dtype=np.float32))
+        else:
+            current_all_present = all_present + [miss]
+            inner_map_item = {}
+            for idx, it in enumerate(current_all_present):
+                inner_map_item[it] = idx
+            train = pd.read_csv(f'data/{args.data}/train_indexed.tsv', sep='\t', header=None)
+            train = train[train[1].isin(current_all_present)]
 
-    # feat prop on textual features
-    for f in os.listdir(f'data/{args.data}/textual_embeddings_indexed'):
-        textual_features[int(f.split('.npy')[0]), :] = torch.from_numpy(
-            np.load(os.path.join(f'data/{args.data}/textual_embeddings_indexed', f)))
+            inner_map_user = {u: idx for idx, u in enumerate(train[0].unique())}
 
-    non_missing_items = list(set(list(range(num_items_textual))).difference(missing_textual_indexed))
-    propagated_textual_features = textual_features.clone()
+            train[0] = train[0].map(inner_map_user)
+            train[1] = train[1].map(inner_map_item)
 
-    for idx in range(args.layers):
-        print(f'[TEXTUAL] Propagation layer: {idx + 1}')
-        propagated_textual_features = matmul(adj.to(device), propagated_textual_features.to(device))
-        propagated_textual_features[non_missing_items] = textual_features[non_missing_items].to(device)
+            adj = get_item_item(num_items=len(current_all_present))
 
+            textual_input = torch.from_numpy(np.load(os.path.join(textual_folder, f'{miss}.npy')))
+            textual_input = textual_input - torch.from_numpy(textual_features).min(0, keepdim=True)[0]
+            textual_input = textual_input / (torch.from_numpy(textual_features).max(0, keepdim=True)[0] -
+                                             torch.from_numpy(textual_features).min(0, keepdim=True)[0])
+            features = model_impute_visual.message_passing(
+                torch.concat((textual_normalized, textual_input), dim=0).to(device),
+                adj.to(device)
+            )
+            output = model_impute_visual.forward(features[-1].to(device))
+            output = (output * (torch.from_numpy(visual_features).to(device).max(0, keepdim=True)[0] -
+                                torch.from_numpy(visual_features).to(device).min(0, keepdim=True)[0])) + \
+                     torch.from_numpy(visual_features).to(device).min(0, keepdim=True)[0]
+            np.save(os.path.join(output_visual, f'{miss}.npy'), output.detach().cpu().numpy())
+
+    # impute missing textual from visual
     for miss in missing_textual_indexed:
-        np.save(os.path.join(output_textual, f'{miss}.npy'), propagated_textual_features[miss].detach().cpu().numpy())
+        if miss in missing_visual_indexed:
+            np.save(os.path.join(output_textual, f'{miss}.npy'), np.zeros(textual_shape, dtype=np.float32))
+        else:
+            current_all_present = all_present + [miss]
+            inner_map_item = {}
+            for idx, it in enumerate(current_all_present):
+                inner_map_item[it] = idx
+            train = pd.read_csv(f'data/{args.data}/train_indexed.tsv', sep='\t', header=None)
+            train = train[train[1].isin(current_all_present)]
+
+            inner_map_user = {u: idx for idx, u in enumerate(train[0].unique())}
+
+            train[0] = train[0].map(inner_map_user)
+            train[1] = train[1].map(inner_map_item)
+
+            adj = get_item_item(num_items=len(current_all_present))
+
+            visual_input = torch.from_numpy(np.load(os.path.join(visual_folder, f'{miss}.npy')))
+            visual_input = visual_input - torch.from_numpy(visual_features).min(0, keepdim=True)[0]
+            visual_input = visual_input / (torch.from_numpy(visual_features).max(0, keepdim=True)[0] -
+                                           torch.from_numpy(visual_features).min(0, keepdim=True)[0])
+
+            features = model_impute_textual.message_passing(
+                torch.concat((visual_normalized, visual_input), dim=0).to(device),
+                adj.to(device)
+            )
+            output = model_impute_textual.forward(features[-1].to(device))
+            output = (output * (torch.from_numpy(textual_features).to(device).max(0, keepdim=True)[0] -
+                                torch.from_numpy(textual_features).to(device).min(0, keepdim=True)[0])) + \
+                     torch.from_numpy(textual_features).to(device).min(0, keepdim=True)[0]
+            np.save(os.path.join(output_textual, f'{miss}.npy'), output.detach().cpu().numpy())
